21d5cba156e1405e7e8500b81bd632b9a718c867
Angelo <ramuzuconcepcion@gmail.com>
2025-05-26 15:43:24 +0800

    feat: streamline build process and add postbuild script for 404.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,19 +9,7 @@ export default defineConfig({
   server: {
     host: true,
   },
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "dist/index.html",
-          dest: "dist",
-          rename: "404.html",
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: [
       {
@@ -32,10 +19,4 @@ export default defineConfig({
     ],
   },
   assetsInclude: ["**/*.JPG", "**/*.PNG"],
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: "index.html",
-    },
-  },
 });
